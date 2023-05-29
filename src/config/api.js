e0aad5ded56fd49f1c6f68445151abd066b93050
Umair <ua071567@gmail.com>
2023-05-29 12:09:26 +0500

    init

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-export const baseURL = 'http://13.233.206.176:4400';
+export const baseURL = 'http://13.233.194.150:4400';
 // export const baseURL = 'http://localhost:4400';
-
-
 
 const Axios = axios.create({
     baseURL : `${baseURL}/api` ,
